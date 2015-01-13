7e345e2d005e9e6a670613b103cdf4ec9159b841
Marcin Drozd <marcindrozd@me.com>
2015-01-13 17:30:38 +0100

    Game creates shuffled deck, deals 2 cards to dealer and player and displays cards in hands.

--- main.rb
+++ main.rb
@@ -7,22 +7,47 @@ use Rack::Session::Cookie,  :key => 'rack.session',
                             :secret => 'aih3284hreuwf8'
 
 get '/' do
-  erb :hello
+  session.clear
+  redirect "/new_game"
 end
 
 get '/new_game' do
   if session[:player_name].nil?
     erb :set_username
   else
-    redirect "/bet"
+    redirect "/game"
   end
 end
 
 post '/new_game' do
   session[:player_name] = params[:player_name]
-  redirect "/bet"
+  redirect "/game"
 end
 
-get '/bet' do
-  "Please place your bets #{session[:player_name]}!"
+get '/game' do
+  VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+  SUITS = ["D", "H", "C", "S"]
+
+  def prepare_deck
+    session[:deck] = []
+    SUITS.each do |suit|
+      VALUES.each do |value|
+        session[:deck] << value + suit
+      end
+    end
+    session[:deck].shuffle!
+  end
+
+  prepare_deck
+
+  session[:player_cards] = []
+  session[:dealer_cards] = []
+
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+
+  erb :display_game
+
 end
